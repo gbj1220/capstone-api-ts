d0905add8ddd60e1fbc6236d89b5ebb4f796e8fd
Gregory.Johnson <3531op@gmail.com>
2022-04-11 13:47:54 -0400

    Changed db connection into a function.

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,21 @@ import cors from "cors";
 import { router as userRoutes } from "./router/userRoutes";
 import "dotenv/config";
 
-if (!process.env.MONGO_URL) {
-  process.exit(1);
-}
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MONGO DB CONNECTED");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const connect_db = () => {
+  if (!process.env.MONGO_URL) {
+    process.exit(1);
+  }
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("MONGO DB CONNECTED");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+// Call function to connect to MongoDB.
+connect_db();
 
 const app = express();
 const PORT = 8080;
